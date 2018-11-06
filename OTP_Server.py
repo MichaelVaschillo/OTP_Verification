62942ee1d5fdf6ebc8dfdbc412526037d21ef1f9
MichaelVaschillo <mishav@uw.edu>
2018-11-06 14:20:55 -0800

    Server done without comments

--- OTP_Server.py
+++ OTP_Server.py
@@ -2,26 +2,10 @@ import hashlib
 import numpy as np
 
 
+
 key = "808670FF00FF00FF08812".encode('utf-8')
 passwordIndex = 0
 #lookUpTable = np.zeros(1000000)
-
-
-#lookUpTable = create_lookup_table(key)
-cont = True
-
-while cont != False:
-    userFindPassword = input("Please intput password: ")
-
-    while(passwordIndex < 1000000):
-        if(create_otp(key) == userFindPassword):
-            userCheckPassword = input("Please get the next password and input it here: ")
-            if(create_otp(key) == userCheckPassword)
-
-
-
-# create_lookup_table(self, key):
-#    print('stub')
 
 def create_otp(input_key):
     key_hash = str(hashlib.sha256(input_key).hexdigest())
@@ -34,3 +18,29 @@ def create_otp(input_key):
     otp = str(int(key_hash,16))[:6]
 
     return otp
+
+
+#lookUpTable = create_lookup_table(key)
+cont = True
+
+while cont != False:
+    userFindPassword = input("Please intput password: ")
+
+    correct = False
+    while(passwordIndex < 1000000 and correct == False):
+        if(create_otp(key) == userFindPassword):
+            userCheckPassword = input("Please get the next password and input it here: ")
+            if(create_otp(key)) == userCheckPassword:
+                print("Correct password")
+                correct = True
+                cont = False
+            else:
+                print("Incorrect password please generate the next password and try again")
+                #cont = False
+
+
+
+
+
+# create_lookup_table(self, key):
+#    print('stub')
