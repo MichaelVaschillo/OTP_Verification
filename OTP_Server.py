1df72634361f3bd407ce74fd8c3c53fba3337e27
MichaelVaschillo <mishav@uw.edu>
2018-11-07 20:49:05 -0800

    Comments to server added

--- OTP_Server.py
+++ OTP_Server.py
@@ -8,10 +8,13 @@ passwordIndex = 0
 #lookUpTable = np.zeros(1000000)
 
 def create_otp(input_key):
+    #Create hash of the key
     key_hash = str(hashlib.sha256(input_key).hexdigest())
 
     global key
 
+    #Set global key variable to the hash of the key
+    #This will be used for the next password generation
     key = key_hash.encode('utf-8')
 
     #Creates a string OTP of key hash with the first 6 digits
@@ -23,39 +26,40 @@ def create_otp(input_key):
     return otp
 
 
-#lookUpTable = create_lookup_table(key)
-#cont = True
 
+#will loop forever since server must always be active
 while True:
+    #intitial password request
     userFirstPassword = input("Please input password: ")
     if(userFirstPassword == create_otp(key)):
+        #if its a match to the next key then yay, access granted
         print("Correct password")
     else:
+        #--------temp variables-------------------------------------
         correct = False
-        revertKey = key
-        revertIndex = passwordIndex
+        revertKey = key                 #Saves current key incase we need to revert
+        revertIndex = passwordIndex     #Saves current passwordIndex incase we need to revert
+        #----------------------------------------------------------
+
+        #Seach ahead for password
         while(passwordIndex < 1000000 and correct == False):
             if(userFirstPassword == create_otp(key)):
-            #if(create_otp(key) == userFirstPassword):
+                #If found ask user for confirmation password
                 userSecondPassword = input("Please get the next password and input it here: ")
+                #If password is correct, give access
                 if(create_otp(key)) == userSecondPassword:
                     print("Correct password")
                     correct = True
                 else:
+                    #If password is incorect start all over again
                     print("Incorrect password please generate the next password and try again")
                     key = revertKey
                     passwordIndex = revertIndex
                     correct = True
                     #cont = False
+        #if first password never found deny access and start over again
         if(passwordIndex >= 1000000):
-            print("Incorrect password")
+            print("Incorrect password please generate the next password and try again")
             key = revertKey
             passwordIndex = revertIndex
         #key = revertKey
-
-
-
-
-
-# create_lookup_table(self, key):
-#    print('stub')
